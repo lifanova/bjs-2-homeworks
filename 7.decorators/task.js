54d33b68493fb3016b7c2f5295a55ecb7b5027ec
lifanova <veralifanovaa@gmail.com>
2021-06-15 15:25:19 +0300

    Task 7

--- 7.decorators/task.js
+++ 7.decorators/task.js
@@ -1,15 +1,44 @@
-function compareArrays(arr1, arr2) {
-  let result;
+function cachingDecoratorNew(func) {
+  let cache = [];
 
-  // Ваш код
+  function wrapper(...args) {
+    //hash есть array
+    const hash = args.join(',');
+    let idx = cache.findIndex((item) => item.hash === hash);
 
-  return result; // boolean
+    if (idx !== -1) {
+      console.log("Из кэша: " + cache[idx].value);
+      return "Из кэша: " + cache[idx].value;
+    } else {
+      let result = func(...args);
+
+      // если длина кэша >= 5, то обрезаем его
+      if (cache.length >= 5) {        
+        cache.shift();
+      }
+
+      //Добавляем ключ - значение в кэш
+      let item = {
+        hash: hash,
+        value: result
+      };
+      cache.push(item);
+
+      console.log("Вычисляем: " + result);
+      return "Вычисляем: " + result;
+    }
+  }
+
+  return wrapper;
 }
 
-function advancedFilter(arr) {
-  let resultArr;
 
+
+
+function debounceDecoratorNew(func) {
   // Ваш код
+}
 
-  return resultArr; // array
+function debounceDecorator2(func) {
+  // Ваш код
 }
